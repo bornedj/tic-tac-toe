6286d63e012543b6ac84e68e43600d82cf983c35
bornedj <bornedj@miamioh.edu>
2021-05-21 12:33:29 -0400

    added update name and update win methods to player class

--- player.py
+++ player.py
@@ -6,6 +6,14 @@ class Player():
         self.name = name
         
     def __repr__(self):
-        return f"Player is {self.name}."
+        return f"Player is {self.name} and has won {self.wins} time(s)."
+        
+    def update_wins(self):
+        self.wins += 1
+
+    def update_name(self, new_name):
+        self.name = new_name
+
+    
     
         
